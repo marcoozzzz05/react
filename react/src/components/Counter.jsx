1cb645c5ab4b4308d8d908e27e36b8f84ea28b7f
marcoozzzz05 <andronico.marco05@gmail.com>
2025-02-26 10:41:32 +0100

    add useState

--- react/src/components/Counter.jsx
+++ react/src/components/Counter.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
+
 export default function Counter() {
+    const [counter, setCounter] = useState(0)
     return (
         <>
-        <div>Counter: 0</div>
+        <div>Counter: {counter}</div>
         </>
     )
 }
