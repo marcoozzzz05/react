4da11255684bc4443b1aa09913c99d1a362313a3
marcoozzzz05 <andronico.marco05@gmail.com>
2025-02-26 12:06:34 +0100

    add button Decrement

--- react/src/components/Counter.jsx
+++ react/src/components/Counter.jsx
@@ -9,10 +9,17 @@ export default function Counter() {
         })
     }
 
+    function Decrement() {
+        setCounter((_counter) => {
+            return _counter - 1
+        })
+    }
+
     return (
         <>
         <div>Counter: {counter}</div>
         <button onClick={Increment}>Increment</button>
+        <button onClick={Decrement}>Decrement</button>
         </>
     )
 }
