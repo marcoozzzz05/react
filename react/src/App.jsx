f8d6c1091016bd06fc527a944acae736cb705d1f
marcoozzzz05 <andronico.marco05@gmail.com>
2025-02-26 10:36:32 +0100

    add counter

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import HelloWord from './components/HelloWord'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,35 +9,7 @@ function App() {
     ]
   return (
     <>
-      <div>
-        {title.map((el,index) => {
-          return <div key={index}>
-            <HelloWord title={el.title} scrivania="ciao"/>
-            <p>calzini</p>
-            </div>
-        })}
-        <HelloWord title="oggi è venerdi"/>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <HelloWord title="sedia"/>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <HelloWord title="cipolla"/>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Counter />
     </>
   )
 }
---
+++ react/src/components/Counter.jsx
@@ -0,0 +1,7 @@
+export default function Counter() {
+    return (
+        <>
+        <div>Counter: 0</div>
+        </>
+    )
+}
