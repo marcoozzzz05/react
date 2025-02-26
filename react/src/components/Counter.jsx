2b9f0c790feba773a90b7658036b21a1ea42932b
marcoozzzz05 <andronico.marco05@gmail.com>
2025-02-26 11:04:18 +0100

    add button Increment

--- react/src/components/Counter.jsx
+++ react/src/components/Counter.jsx
@@ -2,9 +2,17 @@ import { useState } from 'react'
 
 export default function Counter() {
     const [counter, setCounter] = useState(0)
+
+    function Increment() {
+        setCounter((_counter)=> {
+            return _counter + 1
+        })
+    }
+
     return (
         <>
         <div>Counter: {counter}</div>
+        <button onClick={Increment}>Increment</button>
         </>
     )
 }
