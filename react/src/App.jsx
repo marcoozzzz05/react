ba299bf4c12295f0c4a1247b1a04de0325ddf2f0
marcoozzzz05 <andronico.marco05@gmail.com>
2025-02-28 18:49:43 +0100

    add content in Card

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,6 +4,7 @@ import TextInput from './components/TextInput'
 import LoginForm from './components/LoginForm'
 import UncontrolledInput from './components/UncontrolledInput'
 import ItemsList from './components/ItemsList'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +20,10 @@ function App() {
     <LoginForm />
     <UncontrolledInput />
     <ItemsList items={people}/>
+    <Card>
+      <h2>Prova Card</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed in omnis adipisci odio ipsum quisquam, ex optio quod dolorem repellat!</p>
+    </Card>
     </>
   )
 }
