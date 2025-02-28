174d3f8eace625af3bba60d56052f0cdf5effb1b
marcoozzzz05 <andronico.marco05@gmail.com>
2025-02-28 10:53:05 +0100

    add handleInput and OnSubmit

--- react/src/components/LoginForm.jsx
+++ react/src/components/LoginForm.jsx
@@ -3,18 +3,34 @@ import { useState, useEffect } from "react";
 export default function LoginForm() {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    
+    function handleInput(e) {
+        if (e.target.name == "username") {
+            setUsername(e.target.value)
+            console.log(username)
+        } else {
+            setPassword(e.target.value)
+            console.log(password)
+        }
+    }
 
-    function handleInput() {
-        
+    function onSubmit(e) {
+        e.preventDefault()
+        if (password === "" || username ==="") {
+            alert("Compila i campi")
+            return
+        }
+        alert(`Username: ${username} Password: ${password}`)
     }
 
     return (
         <>
-            <form action="">
+            <form onSubmit={onSubmit}>
                 <label htmlFor="">Username:</label>
                 <input type="text" name="username" onInput={handleInput}/>
                 <label htmlFor="">Password:</label>
                 <input type="password" name="password" onInput={handleInput}/>
+                <button type="submit">Login</button>
             </form>
         </>
     )
