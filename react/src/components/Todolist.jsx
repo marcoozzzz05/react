0f3b83b71573bed97e6840edbeacce115fc1dec1
marcoozzzz05 <andronico.marco05@gmail.com>
2025-03-07 14:04:04 +0100

    edit useFilteredTodos and Todolist

--- react/src/components/Todolist.jsx
+++ react/src/components/Todolist.jsx
@@ -1,16 +1,24 @@
 
+import { useState } from "react";
 import useFetch from "../hooks/useFetch"
+import useFilteredTodos from "../hooks/useFilteredTodos";
 
 export default function Todolist() {
     const { data, loading, stateError } = useFetch("https://jsonplaceholder.typicode.com/todos")
+    const [searchTerm, setSearchTerm] = useState("")
+
+    const {filteredTodos} = useFilteredTodos(data, searchTerm)
+
     if (loading) return <p>Caricamento...</p>;
     if (stateError) return <p>Errore: {stateError}</p>;
+
     return (
         <>
+            <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
             <ul>
-                {data && 
-                    data.map(todo => (
-                    <li>{todo.title}</li>
+                {filteredTodos &&
+                    filteredTodos.map(todo => (
+                    <li key={todo.id}>{todo.title}</li>
                 ))}
             </ul>
         </>
--- react/src/hooks/useFilteredTodos.jsx
+++ react/src/hooks/useFilteredTodos.jsx
@@ -4,15 +4,17 @@ export default function useFilteredTodos(todosList, searchTerm) {
     const [filteredTodos, setFilteredTodos] = useState([]);
 
     useEffect(() => {
-        if (todosList) {
+        if (searchTerm) {
             const results = todosList.filter(todo =>
                 todo.title.toLowerCase().includes(searchTerm.toLowerCase())
             );
             setFilteredTodos(results);
+        } else {
+            setFilteredTodos(todosList)
         }
     }, [todosList, searchTerm]);
 
-    return filteredTodos;
+    return {filteredTodos};
 }
 
 
