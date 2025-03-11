b56c8e2279089af26d65c7cb8f9ff5f9b9e50785
marcoozzzz05 <andronico.marco05@gmail.com>
2025-03-11 15:43:07 +0100

    addLink

--- react/src/App2.jsx
+++ react/src/App2.jsx
@@ -1,10 +1,12 @@
 import { Route,Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Navbar from "./components/Navbar"
 
 const App2 = () => {
     return (
         <>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="/about" element={<About />}/>
