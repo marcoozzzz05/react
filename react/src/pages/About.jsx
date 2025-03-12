6bcc609f4b8660a87f445b4c217843011f3f4bce
marcoozzzz05 <andronico.marco05@gmail.com>
2025-03-12 10:59:45 +0100

    add useNavigate

--- react/src/pages/About.jsx
+++ react/src/pages/About.jsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom"
+
 const About = () => {
+    const navigate = useNavigate()
+
+    function toHome() {
+        navigate("/")
+    }
+
     return (
         <>
             <h1>About</h1>
             <div>Questa applicazione permette di visualizzare le to-do</div>
+            <button onClick={toHome}>Home</button>
         </>
     )
 }
